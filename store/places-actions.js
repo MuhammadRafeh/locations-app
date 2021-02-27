0c6d5c133b6654ddda63fe857d7b801426aab382
MuhammadRafeh <muhammadrafehatique@gmail.com>
2021-02-27 17:11:35 +0300

    Storing images on file system

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,22 @@
+import * as FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
-export const addPlace = (title, imageUri ) => { //both params are string
-  return { type: ADD_PLACE, placeData: { title, imageUri } };
+export const addPlace = (title, image) => { //both params are string
+  return async dispatch => {
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName; //document directory delete when app uninstall
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title, imageUri: newPath } });
+  };
 };
